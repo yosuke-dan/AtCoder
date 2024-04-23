c6853d05ee18b86d7ef2f775e53431ec1a102f60
yosuke <zrbnddr@gmail.com>
2024-04-24 00:04:15 +0900

    solved B - Adjacency Matrix

--- B - Adjacency Matrix.py
+++ B - Adjacency Matrix.py
@@ -1,12 +1,17 @@
 N = int(input())
 
 matrix = []
+result = []
 
 for i in range(N):
     matrix.append(list(map(int,input().split())))
 
-print(matrix)
-
 for i in range(N):
-    print()
-    
+    result.append([])
+    for j in range(N):
+        if matrix[i][j] == 1:
+            result[i].append(j+1)
+    print(*result[i])
+
+# 多次元配列を扱う
+# 要素をスペース区切りで出力したい時は*
